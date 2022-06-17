e6587dac24acfc07d1f6c12e32ba18eeab491456
Kelly (Yutong) He <kellyyhe@cs.stanford.edu>
2022-06-16 19:25:16 -0700

    Update test_fmow.py

--- test_fmow.py
+++ test_fmow.py
@@ -19,7 +19,6 @@ from tqdm import tqdm
 
 import model
 from dataset import FMoWSentinel2Path, FSAllPatch
-from calculate_fid import calculate_fid
 from distributed import get_rank, synchronize, reduce_loss_dict
 from tensor_transforms import convert_to_coord_format
 import torchvision.models as models
